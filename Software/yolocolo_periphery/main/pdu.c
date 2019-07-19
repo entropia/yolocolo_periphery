1a7e706ac2b1288d94532061cee0493be3417cfa
GityUpNow <kim-cheng@gmx.de>
2019-07-19 17:08:37 +0200

    Wrote (hacky) function to create a set-command "string"

--- Software/yolocolo_periphery/main/pdu.c
+++ Software/yolocolo_periphery/main/pdu.c
@@ -1,10 +1,22 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "pdu.h"
 
+//returns new pos
+size_t pad0(size_t count, size_t pos, char *dst)
+{
+	for (size_t i = 0; i < pos + count; i++) {
+		dst[pos + i] = 0;	
+	}
+
+	return pos + count;
+}
+
 //Add's start and stop byte to frame
-size_t completeFrame(char* src, size_t srcSize, char** dst) {
+size_t completeFrame(char* src, size_t srcSize, char** dst)
+{
 	if(srcSize == 0) {
 		return 0;
 	}
@@ -17,4 +29,66 @@ size_t completeFrame(char* src, size_t srcSize, char** dst) {
 	*(*dst + 1 + srcSize) = STOP_BYTE;
 
 	return dstSize;
+}
+
+unsigned short calculateChecksum(char* src, size_t size) 
+{
+	if (size == 0) {
+		printf("Checksum error: Length of src is zeor");
+		return 0;
+	}
+
+	unsigned short checksum = src[0];
+	for (size_t i = 1; i < size; i++) {
+		checksum ^= src[i];
+	}
+
+	return checksum;
+}
+
+//Only produces the string to be printed
+size_t commandSet(unsigned char address, char* name, size_t nameSize,
+		  	unsigned short switchState, unsigned short lowAlarm,
+		 	unsigned highAlarm, char *dst)
+{
+	if (address == 0 || address > 8) {
+		printf("Invalid address (%u)", address);
+		return 0;
+	}
+
+	if (nameSize == 0) {
+		printf("Name has a length of zero");
+		return 0;
+	} else if (nameSize > NAME_LENGTH) {
+		printf("Name too long");
+	}
+
+	size_t pos = 0;
+	dst[pos] = SET_BYTE;
+	pos++;
+
+	sprintf(&dst[pos], "%02hu", address);
+	pos += ADDRESS_LENGTH;
+
+	strcpy(name, &dst[pos]);
+	pos += nameSize;
+	pos = pad0(NAME_LENGTH - nameSize, pos, dst);
+
+	sprintf(&dst[pos], "%02hu", switchState);
+	pos += STATE_LENGTH;
+
+	pos = pad0(SET_FIRST_UNKNOWN_LENGTH, pos, dst);
+	
+	sprintf(&dst[pos], "%hX", lowAlarm);
+	pos++;
+
+	pos = pad0(SET_SECOND_UNKNOWN_LENGTH, pos, dst);
+
+	sprintf(&dst[pos], "%hX", highAlarm);
+	pos++;
+
+	unsigned short checksum = calculateChecksum(dst, SET_COMMAND_LENGTH - CHECKSUM_LENGTH);
+
+	sprintf(&dst[pos], "%hX", checksum);
+	return SET_COMMAND_LENGTH;
 }
